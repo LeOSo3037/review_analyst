c9f3b3f872fc632e63d519e8cb67f8957e07b026
LeO So <leoso3037@gmail.com>
2019-07-06 18:59:33 +0800

    filtering

--- amazon.py
+++ amazon.py
@@ -14,4 +14,17 @@ for d in data: # name every review as d
 	sum_len = sum_len + len(d)
 print('The average length of 1 million reviews is', sum_len/len(data))
 
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('There are total', len(new), 'of reviews that contain 100 words or less')
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('There are total', len(good), 'of reviews that contain a word ''good'' ')
+
+
 
